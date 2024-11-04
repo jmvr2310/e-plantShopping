d33c84eb392eeddd49b1ae7bb20412a0b6d07607
jose <jmvr.2310@gmail.com>
2024-11-04 13:51:00 -0500

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,14 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        }));
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
